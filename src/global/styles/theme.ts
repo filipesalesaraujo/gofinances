fa174a60ecfdfbb48614c822117fd02a6c9a3bac
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2022-02-06 13:18:15 -0300

    Criando estilos globais

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  colors: {
+    primary: {
+      main: "#5636d3",
+    },
+    secondary: {
+      main: "#ff872c",
+      light: "rgba(255,133,44,0.3)",
+    },
+    success: {
+      main: "#12A454",
+      light: "rgba(18, 164, 84, 0.5)",
+    },
+    attention: {
+      main: "#e83f3b",
+      light: "rgba(232,63,91,0.5)",
+    },
+    shape: "#fff",
+    title: "#363f5f",
+    text: "#962cb2",
+    background: "#f0f2f5",
+  },
+};
